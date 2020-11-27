f1729551a2967b5cf6dd806510d934d3ac2a68df
EuclidesKinto <euclideskinto@gmail.com>
2020-11-27 12:05:08 -0300

    01 - Variáveis e tipos de Dados

---
+++ secao-01/js/js01.js
@@ -0,0 +1,15 @@
+// Variáveis e tipos de Dados
+
+var nome = "Euclides"; // string
+var idade = 99; // int
+var altura = 1.65 // float
+var ativo = true; // boolean
+var clientes = ['Maria', 'José', 'João']; // array
+var cliente = {
+    nome: 'Maria',
+    idade: 27,
+    altura: 1.73,
+    ativo: true
+} // objeto
+
+console.log(cliente.nome);
